b1f54530e419a9360e9ffd1deae59846dfcff842
Sumit Jha <98050174+DevSj17@users.noreply.github.com>
2024-07-22 20:54:36 +0530

    Create Day_1.py

---
+++ Day_1.py
@@ -0,0 +1,9 @@
+#This code contains a simple program of Band name Generator
+#Greetings
+print("Welcome to the Band Name Generator.")
+#Storing the input in street varialbe
+street = input("What's the name of the city you grew up in?\n") 
+#Storing the input in pet variable
+pet = input("What's your pet's name?\n")
+#Combining both variables to create a Band name
+print("Your band name could be " + street + " " + pet)
